4debdd06eedd5357feead649857021a838c2228f
song <keyoos@163.com>
2024-04-17 14:36:51 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <div class="home-container">
+    <div class="title">主题切换</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.home-container {
+  .title {
+    color: $color;
+  }
+}
+</style>
