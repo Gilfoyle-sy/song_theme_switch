f2c5bd3a45ee5409b9ca2313f4040dafb8c30554
song <keyoos@163.com>
2024-04-29 22:12:50 +0800

    修改为hash模式,适配github page

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
-import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
+  // github page使用的是静态资源
+  // 如果使用history模式,资源路径会错,所以改成hash模式
   history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
